4153fdd23f3da1fa040f504789493cf272cdf539
snooowfire <snooowfire@gmail.com>
2024-06-06 22:09:22 +0800

    refactor: upsert functions and enhance Error messages

--- src/error.rs
+++ src/error.rs
@@ -3,18 +3,18 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
-    #[error("JSON RPC client error: {0}")]
+    #[error("Encountered an issue with the JSON RPC client. Details: {0}")]
     JsonRpcClientError(#[from] jsonrpsee::core::client::Error),
-    #[error("Deserialization error: {0}")]
+    #[error("There was a problem deserializing data. Details: {0}")]
     SerdeJsonError(#[from] serde_json::Error),
-    #[error("Failed to fetch raw transaction. Error code: {code}, Message: {message}")]
+    #[error("Failed to fetch the transaction. Error Code: {code}, Message: '{message}'")]
     FailedFetch { code: i32, message: String },
-    #[error("Result for batched JSON-RPC response is not valid hex: {0}")]
+    #[error("The data received in a batched JSON-RPC response is not valid hexadecimal. Details: {0}")]
     FromHexError(#[from] hex::FromHexError),
-    #[error("I/O error: {0}")]
+    #[error("An I/O error occurred. Details: {0}")]
     IoError(#[from] std::io::Error),
-    #[error("Previous output not found: {tx_hash:?}, {index:?}")]
+    #[error("The previous output could not be found for the transaction '{tx_hash:?}' at index {index}. Please check your data.")]
     PreviousOutputNotFound { tx_hash: H256, index: u32 },
-    #[error("Previous output data not found: {tx_hash:?}, {index:?}")]
+    #[error("The data for the previous output of the transaction '{tx_hash:?}' at index {index} could not be found. Please verify your inputs.")]
     PreviousOutputDataNotFound { tx_hash: H256, index: u32 },
 }
