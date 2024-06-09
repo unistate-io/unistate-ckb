8266e630bd117d5e6a50c45be4edc0fab840b07b
snooowfire <snooowfire@gmail.com>
2024-06-09 12:31:17 +0800

    feat: add token_info relation to addresses entity and modify schema
    - Added a new relation, TokenInfo, to the Addresses entity.
    - Modified the token_info table schema by adding a new primary key field (type_id).
    - Updated related functions and traits to reflect the changes in the schema and relations.

--- src/entity/token_info.rs
+++ src/entity/token_info.rs
@@ -5,17 +5,32 @@ use sea_orm::entity::prelude::*;
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq)]
 #[sea_orm(table_name = "token_info")]
 pub struct Model {
+    #[sea_orm(primary_key, auto_increment = false)]
+    pub type_id: String,
     #[sea_orm(column_type = "VarBinary(StringLen::None)")]
     pub transaction_hash: Vec<u8>,
     pub transaction_index: i32,
-    #[sea_orm(primary_key, auto_increment = false)]
-    pub type_id: String,
     pub decimal: i16,
     pub name: String,
     pub symbol: String,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-pub enum Relation {}
+pub enum Relation {
+    #[sea_orm(
+        belongs_to = "super::addresses::Entity",
+        from = "Column::TypeId",
+        to = "super::addresses::Column::Id",
+        on_update = "NoAction",
+        on_delete = "NoAction"
+    )]
+    Addresses,
+}
+
+impl Related<super::addresses::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Addresses.def()
+    }
+}
 
 impl ActiveModelBehavior for ActiveModel {}
