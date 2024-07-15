94b5833ee86e8af61e6df7c59dde119db02aa0f6
Cupnfish <cupnfish@icloud.com>
2024-07-15 11:58:58 +0800

    fix:     Revert "Create xudt_cell and related tables and triggers"
        This reverts the creation of xudt_cell, transaction_outputs_status, and token_info tables,
        along with their associated indexes, triggers, and functions.

--- migrations/0004_xudt.down.sql
+++ migrations/0004_xudt.down.sql
@@ -1,15 +1,19 @@
--- Drop tables in reverse order to avoid dependencies
--- Drop trigger trg_update_is_consumed
-DROP TRIGGER trg_update_is_consumed ON xudt_status_cell;
+-- Drop indexes from token_info fields
+DROP INDEX idx_token_info_type_id;
 
--- Drop function update_is_consumed
-DROP FUNCTION update_is_consumed();
+-- Drop indexes from xudt_cell fields
+DROP INDEX idx_xudt_cell_type_id;
+DROP INDEX idx_xudt_cell_lock_id;
 
--- Drop table xudt_status_cell
-DROP TABLE xudt_status_cell;
-
--- Drop table xudt_cell
-DROP TABLE xudt_cell;
-
--- Drop table token_info
+-- Drop token_info table
 DROP TABLE token_info;
+
+-- Drop trigger and function for updating is_consumed field
+DROP TRIGGER trg_update_is_consumed ON transaction_outputs_status;
+DROP FUNCTION update_is_consumed;
+
+-- Drop transaction_outputs_status table
+DROP TABLE transaction_outputs_status;
+
+-- Drop xudt_cell table
+DROP TABLE xudt_cell;
